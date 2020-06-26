8dd9c01c51500347e4ca87c6fff557a365398a74
steel-a <stell4b@gmail.com>
2020-06-26 10:21:00 -0300

    fixed load's query

--- __init__.py
+++ __init__.py
@@ -37,108 +37,72 @@ class ExecutionControl:
         :return: True if load a candidate
                  False if does not have a candidate to load or error
         """
-        hourMin = re.search(' ([0-9]{2}:[0-9]{2}):[0-9]{2}',
-                            datetime.now().strftime('%Y-%m-%d %H:%M:%S'))[1]
 
         if processName is None:
             queryCandidate = f"""
                 select *
                 from {self.table}
                 where
-                (statusLastExecution != 'S'
-                    and (hourStart <= '{hourMin}' or hourStart2 <= '{hourMin}')
-                    and     hourEnd >= '{hourMin}'
-                ) or
-                (statusLastExecution = 'E')
+                    (
+                    (statusLastExecution = 'S'
+                        /* Time rule */
+                        and (hourStart <= date_format(now(),'%H:%i') or hourStart2 <= date_format(now(),'%H:%i')) and hourEnd >= date_format(now(),'%H:%i')
+                        /* Specific rule for each periodicity */
+                        and (
+                            (periodicity = 'D' and dateLastSuccess < curdate()) or
+                            (periodicity = 'B' and dateLastSuccess < curdate() and dayofweek(curdate()) between 2 and 6) or
+                            (periodicity = 'W' and dateLastSuccess < curdate() and dayofweek(curdate()) = day) or
+                            (periodicity = 'M' and dateLastSuccess < curdate() and ( (day > 0 and dayofmonth(curdate()) = day) or
+                                                                                    (day = -1 and dayofmonth(curdate())=dayofmonth(last_day(curdate())))
+                                                                                )) or
+                            (periodicity = 'Y' and dateLastSuccess < curdate() and ( (day > 0 and dayofyear(curdate()) = day) or
+                                                                                    (day = -1 and dayofyear(curdate())=dayofyear((DATE_ADD(NOW(), INTERVAL 12-MONTH(NOW()) MONTH))))
+                                                                                )))
+                        /* Execute if not executed today, or already exec today but needs to repeat every x minutes */
+                        and (dateLastSuccess!=curdate() or (repeatMinutes>0 and timestampdiff(MINUTE,curdate(),now())>repeatMinutes))
+                        )
+                    or (statusLastExecution = 'E')
+                    or (statusLastExecution = 'P' and timestampdiff(MINUTE,timeLastExecution,now())>30)
+                    )
+                    # Max retries
+                    and (triesWithError < maxTriesWithError or timestampdiff(MINUTE,timeLastExecution,now())>minsAfterMaxTries)
+                    LIMIT 1
                 """
         else:
             queryCandidate = f"""
-                select * from {self.table} where statusLastExecution != 'P'
-                and processName = '{processName}'
+                select * from {self.table} where processName = '{processName}'
                 """
 
 
-        for dic in self.db.getListRows(queryCandidate):
-            if dic is None:
-                return False
+        dic = self.db.getRow(queryCandidate)
+        if dic is None:
+            return False
+        
 
-            # Rules to not execute
-            if processName==None:
-                p = dic['periodicity']
-                today = datetime.now()
-                dtSuccess = dic['dateLastSuccess']
-                dtExec = dic['timeLastExecution']
-                minSinceSuccess = round((today-dtSuccess).seconds/60)
-                minSinceExec = round((today-dtExec).seconds/60)
-                repeat = dic['repeatMinutes']
-                status = dic['statusLastExecution']
-                dayWeek = today.weekday()+1
-                businessDay = (dayWeek <= 5)
-                executedToday = (today.strftime('%Y-%m-%d') == dtSuccess.strftime('%Y-%m-%d'))
-                day = dic['day']
-                tries = dic['triesWithError']
-                maxTries = dic['maxTriesWithError']
-                minsAfterTries = dic['minsAfterMaxTries']
+        self.id = dic['id']
+        self.name = dic['name']
+        self.processName = dic['processName']
+        self.processParam = dic['processParam']
+        self.idUser = dic['idUser']
+        self.periodicity = dic['periodicity']
+        self.day = dic['day']
+        self.hourStart = dic['hourStart']
+        self.hourStart2 = dic['hourStart2']
+        self.hourEnd = dic['hourEnd']
+        self.repeatMinutes = dic['repeatMinutes']
+        self.dateLastSuccess = dic['dateLastSuccess']
+        self.statusLastExecution = dic['statusLastExecution']
+        self.timeLastExecution = dic['timeLastExecution']
+        self.triesWithError = dic['triesWithError']
+        self.maxTriesWithError = dic['maxTriesWithError']
+        self.minsAfterMaxTries = dic['minsAfterMaxTries']
+        self.error = dic['error']
+        self.numHardRegisters = dic['numHardRegisters']
+        self.numHardRegistersLast = dic['numHardRegistersLast']
+        self.numSoftRegisters = dic['numSoftRegisters']
+        self.fk = dic['fk']
 
-                # Repeat = 0 -> execute once a day      # Repeat > 0 -> exec even x minutes
-                if((repeat == 0 and executedToday) or (repeat > 0 and (minSinceSuccess < repeat))):
-                    continue
-
-                # Periodicity criteria
-                elif status == 'S':
-                    if ((p=='B' and not businessDay)
-                    or (p=='W' and day != dayWeek)
-                    or (p=='M' and day != today.date.day)
-                    ):
-                        continue
-
-                # After maxTries, exec every 30 minutes
-                elif status == 'E' and tries >= maxTries and minSinceExec <= minsAfterTries:
-                    continue
-
-            #Verify preRequisites
-            queryMetRequisites = f"""
-                select id from {self.table}
-                 where dateLastSuccess >= '{datetime.now().strftime('%Y-%m-%d')+' 00:00:01'}'
-                   and statusLastExecution = 'S'
-            """
-            lstMetRequisites = self.db.fetchall(queryMetRequisites)
-            self.preRequisites = dic['preRequisites']
-            if self.preRequisites is not None:
-                allReqMet = True
-                for idReq in self.preRequisites.split(';'):
-                    if idReq not in lstMetRequisites:
-                        allReqMet = False
-                if not allReqMet:
-                    continue
-            
-
-            self.id = dic['id']
-            self.name = dic['name']
-            self.processName = dic['processName']
-            self.processParam = dic['processParam']
-            self.idUser = dic['idUser']
-            self.periodicity = dic['periodicity']
-            self.day = dic['day']
-            self.hourStart = dic['hourStart']
-            self.hourStart2 = dic['hourStart2']
-            self.hourEnd = dic['hourEnd']
-            self.repeatMinutes = dic['repeatMinutes']
-            self.dateLastSuccess = dic['dateLastSuccess']
-            self.statusLastExecution = dic['statusLastExecution']
-            self.timeLastExecution = dic['timeLastExecution']
-            self.triesWithError = dic['triesWithError']
-            self.maxTriesWithError = dic['maxTriesWithError']
-            self.minsAfterMaxTries = dic['minsAfterMaxTries']
-            self.error = dic['error']
-            self.numHardRegisters = dic['numHardRegisters']
-            self.numHardRegistersLast = dic['numHardRegistersLast']
-            self.numSoftRegisters = dic['numSoftRegisters']
-            self.fk = dic['fk']
-
-            return True
-
-        return False
+        return True
 
 
     def start(self, executionTime:str=None):
